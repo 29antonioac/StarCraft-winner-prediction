c0e8a70c70a6e6e2bc0b1d3e84a621e60e879d55
Antonio Álvarez Caballero <analca3@gmail.com>
2017-09-08 13:55:48 +0200

    Añadido texto con licencia, closes #20

--- Spark-workspace/StarCraft-Analytics/src/analytics/knn.scala
+++ Spark-workspace/StarCraft-Analytics/src/analytics/knn.scala
@@ -1,8 +1,22 @@
 /*
- * author: Antonio Álvarez Caballero (a.k.a. analca3)
- * license: GPLv3
- * 
- * This is a temporary module. When spark-knn package implements MLWritable, it will be merged to the main modules.
+
+		Copyright (C) 2017 Antonio Álvarez Caballero a.k.a. analca3
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
+  	This is a temporary module. When spark-knn package implements MLWritable, it will be merged to the main modules.
  */
 
 package analytics
